44e2d0823b6677e0d6e6d6fc2434795f2b172ed8
xbrian0610 <xbrian0610@gmail.com>
2020-04-16 18:59:03 +0800

    【front_set】

---
+++ js/admin_news_content.js
@@ -0,0 +1,8 @@
+var tmp_content = document.getElementsByClassName('news_details');
+var get_tmp_html = tmp_content[0].outerHTML;
+console.log(get_tmp_html);
+
+// function edit_content() {
+
+//     $("#editor_news_content").append("123");
+// }
